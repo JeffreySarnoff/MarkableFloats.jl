5ca47367fad0686d54aa23e85275237c0007ae13
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-30 02:07:45 -0400

    Update type.jl

--- src/type.jl
+++ src/type.jl
@@ -20,19 +20,21 @@ markable(x::Float64) = 7.458340731200207e-155 <= x <= 1.3407807929942596e154
    true
 end
 
-Unmarked(x::Float64) = markable(x) && reinterpret(MarkFloat64, x)
-Marked(x::Float64) = markable(x) && mark(reinterpret(MarkFloat64, x))
+@inline unmark(x::MarkFloat64) =
+    reinterpret(MarkFloat64, (reinterpret(UInt64,x) & 0xefffffffffffffff))
+@inline mark(x::MarkFloat64) =
+    reinterpret(MarkFloat64, (reinterpret(UInt64,x) | 0x1000000000000000))
+
+@inline marker(x::MarkFloat64) = (reinterpret(UInt64,x) & 0x1000000000000000)
+
+@inline Unmarked(x::Float64) = markable(x) && reinterpret(MarkFloat64, x)
+@inline Marked(x::Float64) = markable(x) && mark(reinterpret(MarkFloat64, x))
 
 @inline Float64(x::MarkFloat64) = reinterpret(Float64, unmark(x))
 @inline MarkFloat64(x::Float64) = Unmarked(x)
 
-@inline unmark(x::MarkFloat64) = reinterpret(MarkFloat64, (reinterpret(UInt64,x) & ~0x1000000000000000))
-@inline mark(x::MarkFloat64) = reinterpret(MarkFloat64, (reinterpret(UInt64,x) | 0x1000000000000000))
-
 @inline ismarked(x::MarkFloat64) = (reinterpret(UInt64,x) & 0x1000000000000000) === 0x1000000000000000
 @inline isunmarked(x::MarkFloat64) = (reinterpret(UInt64,x) & 0x1000000000000000) === 0x0000000000000000
-
-@inline marker(x::MarkFloat64) = (reinterpret(UInt64,x) & 0x1000000000000000)
 
 string(x::MarkFloat64) = string(Float64(x))
 show(io::IO, x::MarkFloat64) = print(io, string(x))
