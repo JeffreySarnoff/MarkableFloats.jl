0a63c425637c105d01748267b74b9d0dc9a56b67
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-30 01:36:29 -0400

    Update MarkableFloats.jl

--- src/MarkableFloats.jl
+++ src/MarkableFloats.jl
@@ -18,7 +18,12 @@ import Base.Math: zero, one, iszero, isone, isinteger,
     (+), (-), (*), (/), (\), (^),
     round, trunc, ceil, floor,
     div, fld, cld, mod, rem,
-    sqrt, cbrt
+    sqrt, cbrt,
+    log, log2, log10, log1p, exp, expm1,
+    sin, cos, tan, sec, csc, cot, sinpi, cospi,
+    asin, acos, atan, asec, acsc, acot, atan2,
+    sinh, cosh, tanh, sech, csch, coth,
+    asinh, acosh, atanh, asech, acsch, acoth
 
 include("type.jl")
 
