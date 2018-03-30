82438a8eba29fd85f442953758427593cb871bd2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-30 02:20:18 -0400

    Update type.jl

--- src/type.jl
+++ src/type.jl
@@ -1,3 +1,4 @@
+
 abstract type MarkableFloat <: AbstractFloat end
 
 primitive type MarkFloat64 <: MarkableFloat 64 end
@@ -5,7 +6,6 @@ primitive type MarkFloat64 <: MarkableFloat 64 end
 #=
 fr,ex=frexp(realmax(Float64)); ldexp(fr, ex>>1)
 1.3407807929942596e154
-
 fr,ex=frexp(realmin(Float64)); ldexp(fr, ex>>1)
 7.458340731200207e-155
 =#
@@ -20,23 +20,19 @@ markable(x::Float64) = 7.458340731200207e-155 <= x <= 1.3407807929942596e154
    true
 end
 
-@inline unmarkraw(x::MarkFloat64) = reinterpret(UInt64,x) & 0xefffffffffffffff
-@inline markraw(x::MarkFloat64)   = reinterpret(UInt64,x) | 0x1000000000000000
-
-@inline unmark(x::MarkFloat64) = reinterpret(MarkFloat64, unmarkraw(x))
-@inline mark(x::MarkFloat64) =   reinterpret(MarkFloat64, markraw(x))
-
-@inline marker(x::MarkFloat64) = (reinterpret(UInt64,x) & 0x1000000000000000)
-
-@inline Unmarked(x::Float64) = markable(x) && reinterpret(MarkFloat64, x)
-@inline Marked(x::Float64) = markable(x) && mark(reinterpret(MarkFloat64, x))
+Unmarked(x::Float64) = markable(x) && reinterpret(MarkFloat64, x)
+Marked(x::Float64) = markable(x) && mark(reinterpret(MarkFloat64, x))
 
 @inline Float64(x::MarkFloat64) = reinterpret(Float64, unmark(x))
 @inline MarkFloat64(x::Float64) = Unmarked(x)
 
-@inline ismarked(x::MarkFloat64) = (reinterpret(UInt64,x)  ⊻ 0x1000000000000000) === 0x0000000000000000
+@inline unmark(x::MarkFloat64) = reinterpret(MarkFloat64, (reinterpret(UInt64,x) & ~0x1000000000000000))
+@inline mark(x::MarkFloat64) = reinterpret(MarkFloat64, (reinterpret(UInt64,x) | 0x1000000000000000))
+
+@inline ismarked(x::MarkFloat64) = (reinterpret(UInt64,x) & 0x1000000000000000) === 0x1000000000000000
 @inline isunmarked(x::MarkFloat64) = (reinterpret(UInt64,x) & 0x1000000000000000) === 0x0000000000000000
+
+@inline marker(x::MarkFloat64) = (reinterpret(UInt64,x) & 0x1000000000000000)
 
 string(x::MarkFloat64) = string(Float64(x))
 show(io::IO, x::MarkFloat64) = print(io, string(x))
-
