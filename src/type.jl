f3170406511f4c1575d0e49d3096cae1c9f17853
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2025-09-09 21:11:01 -0400

    Update type.jl

--- src/type.jl
+++ src/type.jl
@@ -56,11 +56,17 @@ const Mark1 = 0x01
 const Mark2 = 0x02
 const Mark3 = 0x03
 
+
 const Marker0 = "" # '₀' 
-const Marker1 = '₁' 
+const Marker1 = '₁'
 const Marker2 = '₂'
 const Marker3 = '₃'
-
+#=
+const Marker0 = "" # '₀' 
+const Marker1 = "𝟙" 
+const Marker2 = "𝟚"
+const Marker3 = "𝟛"
+=#
 const Markers = (Marker0, Marker1, Marker2, Marker3)
 
 marker(mark) = Markers[mark + 1]
@@ -117,14 +123,14 @@ with_mark1(x::Float14) = reinterpret(Float15, (reinterpret(UInt16, x) | 0x0001))
 function Base.show(io::IO, x::Float14)
     mark = marker(marking(x))
     value = Float16(unmark(x))
-    str = string(mark, "Float14(", value, ")")
+    str = string("Float14(", value, mark, ")")
     print(io, str)
 end
 
 function Base.show(io::IO, x::Float15)
     mark = marker(marking(x))
     value = Float16(unmark(x))
-    str = string(mark, "Float15(", value, ")")
+    str = string("Float15(", value, ", ", mark, ")")
     print(io, str)
 end
 
